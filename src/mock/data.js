398e3e28ab85ee44be03f7aec22b8e871dbd6d1d
Michaela Laden <michaela.laden@gmail.com>
2020-02-22 00:15:03 -0500

    First commit for portfolio website

---
+++ src/mock/data.js
@@ -0,0 +1,94 @@
+import uuidv1 from 'uuid/v1';
+
+// HEAD DATA
+export const headData = {
+  title: 'Michaela Laden • Portfolio', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my portfolio', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: '',
+  name: 'Michaela Laden',
+  subtitle: 'Web Developer & Data Analyst',
+  cta: 'Procede as Anticipated',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'profile.png',
+  paragraphOne: 'Hello, my name is Michaela. I am nearly a year out of college ',
+  paragraphTwo: '',
+  paragraphThree: '',
+  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: uuidv1(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: '',
+  btn: '',
+  email: 'michaela.laden@gmail.com',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: uuidv1(),
+      name: 'twitter',
+      url: '',
+    },
+    {
+      id: uuidv1(),
+      name: 'codepen',
+      url: '',
+    },
+    {
+      id: uuidv1(),
+      name: 'linkedin',
+      url: '',
+    },
+    {
+      id: uuidv1(),
+      name: 'github',
+      url: '',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+};
